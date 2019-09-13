575836e6bcc23c80242564d9866a6827cd0e0e2b
Nathan <nembery@gmail.com>
2019-09-13 13:52:32 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skilletlib",
+    version="0.0.1",
+    author="Nathan Embery",
+    author_email="nembery@paloaltonetworks.com",
+    description="Tools for working with PAN-OS Skillets in Python 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nembery/skilletlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
