055c8bf845ec1f0391344d36aca7965b047a63c0
Nathan <nembery@gmail.com>
2020-02-20 11:32:00 -0500

    do not set relative xpath for #52

--- skilletlib/panoply.py
+++ skilletlib/panoply.py
@@ -890,8 +890,8 @@ class Panoply:
         for xpath in not_found_xpaths:
 
             set_xpath, entry = self.__split_xpath(xpath)
-            # set_xpath = set_xpath.replace('./', '/config/')
-            set_xpath = re.sub(r'^/config/', r'\./', set_xpath)
+            # do not change xpath to be relative for #52
+            # set_xpath = re.sub(r'^/config/', r'\./', set_xpath)
             full_relative_xpath = re.sub(r'^/config/', r'\./', xpath)
             tag = re.sub(r'\[.*\]', '', entry)
 
