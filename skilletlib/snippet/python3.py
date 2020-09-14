94a57c2e4bd9637c61b9a8c7945e936d7dc76b12
Nathan <nembery@gmail.com>
2020-09-14 14:56:25 -0400

    stub out python3 pass through for #105

--- skilletlib/snippet/python3.py
+++ skilletlib/snippet/python3.py
@@ -8,4 +8,15 @@ class Python3Snippet(Snippet):
     required_metadata = {'name', 'file'}
 
     def execute(self, context):
-        pass
+        """
+        This is a stub function and is NOT currently implemented in skilletlib.
+
+        :param context: The context used for python3 script execution
+        :return: script output and tuple of success / failure
+        """
+        if 'python3_output' in context:
+            # this is admittedly a hack to allow python3 skillets to be executed using other means in other tools
+            # but still use output capturing, output_templates, etc from skilletlib. This is def a FIXME item
+            return context['python3_output'], 'success'
+
+        return '', 'success'
