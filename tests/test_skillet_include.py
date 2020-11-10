f965c06b7ac80027158f8e0241afc4c2a93681ff
Nathan <nembery@gmail.com>
2020-11-10 15:05:18 -0500

    clean up skillet include tests

--- tests/test_skillet_include.py
+++ tests/test_skillet_include.py
@@ -1,6 +1,4 @@
-# This script will load the example configuration found in 'tests/example_config/config.xml'
-# and then execute all the example skillets found in the 'skilletlib/example_skillets' directory.
-
+# This test will use skilletLoader to load a skillet that includes snippets and variables from other skillets
 
 from skilletlib import SkilletLoader
 from skilletlib.skillet.base import Skillet
@@ -9,29 +7,19 @@ from skilletlib.utils.testing_utils import setup_dir
 
 setup_dir()
 
+# create our context that will contain only a 'config' variable
 context = dict()
 with open('example_config/config.xml', 'r') as config:
     context['config'] = config.read()
 
 
-def load_and_execute_skillet(skillet_path: str, skillet_name: str) -> dict:
-    skillet_loader = SkilletLoader(path=skillet_path)
-    skillet = skillet_loader.get_skillet_with_name(skillet_name)
-    print('=' * 80)
-    print(f'Executing {skillet.label}\n'.center(80))
-
-    output = skillet.execute(context)
-
-    if 'pan_validation' in output:
-        for k, v in output.get('pan_validation', {}).items():
-            r = str(v.get('results', 'False'))
-            print(f'{k:60}{r}')
-            assert r == 'True'
-
-    return output
-
-
 def test_get_skillet_by_name():
+    """
+    Test to verify skilletLoader can successfully load all skillets found in the 'skillet_incluedes'
+    directory and return the one with the 'include_other_skillets' name
+
+    :return: None
+    """
     skillet_path = '../example_skillets/skillet_includes/'
     skillet_loader = SkilletLoader(path=skillet_path)
     skillet = skillet_loader.get_skillet_with_name('include_other_skillets')
@@ -39,22 +27,39 @@ def test_get_skillet_by_name():
 
 
 def test_skillet_includes():
+    """
+    Tests to verify the Skillet object is successfully compiled from all included skillets.
+    The 'include_other_skillets' skillet includes snippets and variables from two other skillets
+    in the same directory
+
+    :return: None
+    """
     skillet_path = '../example_skillets/skillet_includes/'
     skillet_loader = SkilletLoader(path=skillet_path)
     skillet: Skillet = skillet_loader.get_skillet_with_name('include_other_skillets')
+    # verify we can find and load the correct skillet
     assert skillet.name == 'include_other_skillets'
+
+    # verify the correct number of snippets. 4 of which are included from other skillets
     assert len(skillet.snippets) == 6
 
     included_snippet: Snippet = skillet.get_snippet_by_name('check_network_profiles')
+
+    # verify we can get an included snippet from the skillet object
     assert included_snippet is not None
+
+    # verify the 'label' metadata attribute has been overridden correctly
     assert included_snippet.metadata.get('label', '') == 'Check Network Profiles Override'
 
     included_variable: dict = skillet.get_variable_by_name('some_update_variable')
+
+    # verify the included variable is present in the compiled skillet
     assert included_variable is not None
+
+    # verify the default value is correctly set from the included variable
     assert included_variable.get('default', '') == 'test123456'
 
 
 if __name__ == '__main__':
     test_get_skillet_by_name()
     test_skillet_includes()
-
