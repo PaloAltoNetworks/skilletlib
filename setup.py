a4e705844ab7b3928c34f9cc5ee2946a421122ed
Nathan <nembery@gmail.com>
2021-07-09 11:39:59 -0400

    1.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skilletlib",
-    version="1.1.7",
+    version="1.1.8",
     author="Nathan Embery",
     author_email="nembery@paloaltonetworks.com",
     description="Tools for working with PAN-OS Skillets in Python 3",
