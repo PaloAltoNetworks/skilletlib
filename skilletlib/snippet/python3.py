6371032e9ac92aac4fb1f6e263c7b89922058446
Emiliano Ticci <emiticci@gmail.com>
2023-12-26 23:14:17 +0100

    Implement python3 snippet execution
    Add a simple routine for execution of python3 snippets. Output is saved also in context in continuity with previous stub.

--- skilletlib/snippet/python3.py
+++ skilletlib/snippet/python3.py
@@ -1,3 +1,4 @@
+import subprocess
 from .base import Snippet
 
 
@@ -9,14 +10,14 @@ class Python3Snippet(Snippet):
 
     def execute(self, context):
         """
-        This is a stub function and is NOT currently implemented in skilletlib.
+        Execute a python3 snippet.
 
         :param context: The context used for python3 script execution
         :return: script output and tuple of success / failure
         """
-        if 'python3_output' in context:
-            # this is admittedly a hack to allow python3 skillets to be executed using other means in other tools
-            # but still use output capturing, output_templates, etc from skilletlib. This is def a FIXME item
+        try:
+            p3_exec = subprocess.run(['/usr/bin/env', 'python3', f'{self.skillet.path}/{self.file}'], check=True, capture_output=True)
+            context['python3_output'] = p3_exec.stdout.decode('utf-8')
             return context['python3_output'], 'success'
-
-        return '', 'success'
+        except subprocess.CalledProcessError:
+            return '', 'failure'
